9c8e5ca08e74e6c74de099013123fe9a4f365e50
Varn <varnard@vp.pl>
2016-11-18 03:43:24 +0100

    step2

--- src/com/company/Handler.java
+++ src/com/company/Handler.java
@@ -8,4 +8,12 @@ public class Handler {
     public double sum(double x, double y){
         return x+y;
     }
+
+    public double avg(Object[] numbers){
+        double avg=0;
+        for (int i = 0; i < numbers.length; i++) {
+            avg+=(Double)numbers[i];
+        }
+        return avg/numbers.length;
+    }
 }
