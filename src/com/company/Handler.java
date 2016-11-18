3f6f921fba194090fde68bc13bf8bb869ddcabcd
Varn <varnard@vp.pl>
2016-11-18 03:27:57 +0100

    step1

---
+++ src/com/company/Handler.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by Varn on 2016-11-18.
+ */
+public class Handler {
+
+    public double sum(double x, double y){
+        return x+y;
+    }
+}
--- src/com/company/JavaServer.java
+++ src/com/company/JavaServer.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.server.XmlRpcServer;
+import org.apache.xmlrpc.webserver.WebServer;
+
 public class JavaServer {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            WebServer webServer = new WebServer(8000);
+            XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer();
+
+            PropertyHandlerMapping phm = new PropertyHandlerMapping();
+            phm.addHandler("handler", Handler.class);
+            xmlRpcServer.setHandlerMapping(phm);
+
+            webServer.start();
+
+            System.out.println("Started successfully.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
